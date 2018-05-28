046dd3c3b8a5f7004e5122cb6e1499ff6acae338
Nikhil Chelani <nikhilchelani.94@gmail.com>
2018-05-28 18:12:16 +0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}!`);
+});
