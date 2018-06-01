8bc2fe04d4c3d818b3a7fbc085f7f538737a1beb
Nikhil Chelani <nikhilchelani.94@gmail.com>
2018-06-01 19:30:42 +0400

    Greet new users & alert others

--- server/server.js
+++ server/server.js
@@ -19,14 +19,32 @@ io.on('connection', socket => {
 
     console.log('New user connected!');
 
-    socket.emit('newMessage', {
-        from: 'mike@example.com',
-        text: 'Whats up!!',
-        createdAt: 123
-    });
+    socket.on('createMessage', (message) => {
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('createMessage', newMessage);
+        socket.emit('newMessage', {
+            from: 'Admin',
+            text: 'Welcome to the chat app!',
+            createdAt: new Date().getTime()
+        });
+
+        socket.broadcast.emit('newMessage', {
+            from: 'Admin',
+            text: `New user joined!`,
+            createdAt: new Date().getTime()
+        });
+        
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+
     });
 
     socket.on('disconnect', () => {
