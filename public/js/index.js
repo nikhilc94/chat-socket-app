af3c02715aa67a4ba4ac8a5690b42044c97f1d45
unknown <nikhilchelani.94@gmail.com>
2018-06-27 16:36:21 +0400

    Added autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+function scrollToBottom() {
+    
+    // Selectors 
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+};
 
 socket.on('connect', function () {
     console.log('Connected to server!')
@@ -21,6 +39,7 @@ socket.on('newMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 
 });
 
@@ -35,16 +54,17 @@ socket.on('newLocationMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 
 });
 
 // On form submit, emit createMessage event with form data.
 $('#message-form').on('submit', function (e) {
-   
+
     e.preventDefault();
 
     var messageTextbox = $('[name=message]')
-    
+
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextbox.val()
